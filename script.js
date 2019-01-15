207c9ea6d31479bd97199f34d55a67f2732f4e08
annaPelczarska <anna.pelczarska@gmail.com>
2019-01-15 14:19:19 +0100

    Update v2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://mighty-meadow-72996.herokuapp.com/v1/task/';
+  const apiRoot = 'https://mighty-meadow-72996.herokuapp.com/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
